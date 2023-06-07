23841ff43c925d53019e83b03bc3cd9ea527350e
tursunpulat2002 <tursunpulat2002@gmail.com>
2023-06-07 19:01:11 -0400

    Initial commit set up project.

---
+++ Culinary Delights/Culinary Delights/Culinary_DelightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Culinary_DelightsApp.swift
+//  Culinary Delights
+//
+//  Created by Tursunpulat Orolov on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Culinary_DelightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
